4d325d6ca2b52df630198b94e05ed42149ad47c1
Tamme Schichler <tamme@schichler.dev>
2020-11-08 19:47:53 +0100

    Only apply `#[track_caller]` on Rust 1.46 or later

--- src/lib.rs
+++ src/lib.rs
@@ -45,7 +45,7 @@ pub mod readme {
 ///
 /// [`syn::parse2`]: https://docs.rs/syn/1.0.14/syn/fn.parse2.html
 /// [`syn::parse::ParseBuffer::call`]: https://docs.rs/syn/1.0.14/syn/parse/struct.ParseBuffer.html#method.call
-#[track_caller]
+#[rustversion::attr(since(1.46), track_caller)]
 pub fn call2<T, P: FnOnce(ParseStream) -> T>(input: TokenStream, parser: P) -> T {
 	let mut result: Option<T> = None;
 	Parser::parse2(
